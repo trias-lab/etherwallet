4c565ae1ab0004d1431bb9c80ca229b721f5ed45
dasenlinCode <2845293434@qq.com>
2018-09-25 18:42:21 +0800

    modify tri method list

--- wallet/views/tri.py
+++ wallet/views/tri.py
@@ -11,6 +11,33 @@ def tri(request):
     postBody = request.body
     headers = {'Content-Type': 'application/json; charset=UTF-8'}
 
+    supportedMethods = set(['eth_blockNumber',
+                            'net_version',
+                            'eth_getBalance',
+                            'eth_getTransactionByHash',
+                            'eth_gasPrice',
+                            'eth_getTransactionCount',
+                            'eth_sendRawTransaction',
+                            'eth_estimateGas'])
+    try:
+        reqDict = json.loads(postBody)
+        method = reqDict["method"]
+        if not method:
+            raise (Exception("method not find"))
+        if not method in supportedMethods:
+            raise (Exception("method not supported %s" % method))
+    except Exception as e:
+        logger.error(e)
+        err = {
+            "jsonrpc": "2.0",
+            "id": 1,
+            "error": {
+                "code": -1,
+                "message": "not support method"
+            }
+        }
+        return JsonResponse(err)
+
     with open(CONF_JSON, 'r') as conf:
         rec = conf.read()
     records = json.loads(rec)
